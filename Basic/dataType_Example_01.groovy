ec4a555c73f7974295dfe8a3eeb0560e9a6c7bc9
Chee Wai Wong <academicsnotes@gmail.com>
2022-08-02 21:25:08 +0800

    Commit scripts

---
+++ Basic/dataType_Example_01.groovy
@@ -0,0 +1,14 @@
+class dataType_example_01{
+
+    static void main(String[] args){
+
+        int x = 5;
+        long y = 100L;
+        float a = 10.56f;
+        double b = 10.5e40;
+        BigInteger bi = 30g;
+        BigDecimal bd = 3.5g;
+
+        println("x: " + x + " y: " + y + " a: " + a + " bi: " + bi + " bd: " + bd);
+    }
+}
--- Basic/helloworld.groovy
+++ Basic/helloworld.groovy
@@ -3,5 +3,7 @@
 class Example{
     static void main(String[] args){
        println "Hello World!"; 
+       def x = 10;
+       println("x: " + x); 
     }
 }
